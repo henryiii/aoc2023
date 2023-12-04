23e02916ae935ba4e2f2b1c58f710ceb679c6ed1
Henry Schreiner <henryschreineriii@gmail.com>
2023-12-04 18:22:39 -0500

    refactor: clean up impl
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/01/main.rs
+++ src/bin/01/main.rs
@@ -2,20 +2,20 @@
 
 use std::io::prelude::*;
 
-fn index_to_int(line: &str, value: usize) -> u32 {
+fn index_to_int(line: &str, value: usize) -> Option<u32> {
     let result = line.as_bytes()[value] as char;
-    result.to_digit(10).unwrap()
+    result.to_digit(10)
+}
+
+fn number_line(line: &str) -> Option<u32> {
+    let start = line.find(|c: char| c.is_ascii_digit())?;
+    let end = line.rfind(|c: char| c.is_ascii_digit())?;
+    Some(10 * index_to_int(line, start)? + index_to_int(line, end)?)
 }
 
 fn main() {
-    let mut sum = 0;
     let file = std::fs::File::open("resources/01/input.txt").unwrap();
     let lines = std::io::BufReader::new(file).lines();
-    for _line in lines {
-        let line = _line.unwrap();
-        let start = line.find(|c: char| c.is_ascii_digit()).unwrap();
-        let end = line.rfind(|c: char| c.is_ascii_digit()).unwrap();
-        sum += 10 * index_to_int(&line, start) + index_to_int(&line, end);
-    }
+    let sum = lines.fold(0, |x, line| x + number_line(&line.unwrap()).unwrap());
     println!("Sum: {sum}");
 }
