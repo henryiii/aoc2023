940e903d63d5dc3a30f225370d7d0f10a8b7e86c
Henry Schreiner <henryschreineriii@gmail.com>
2023-12-04 22:14:54 -0500

    refactor: simpler
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/01b/main.rs
+++ src/bin/01b/main.rs
@@ -2,17 +2,16 @@
 
 use std::io::prelude::*;
 
-fn index_to_int(line: &str, value: usize) -> Option<u32> {
-    let result = line.as_bytes()[value] as char;
+fn index_to_int(line: &[u8], value: usize) -> Option<u32> {
+    let result = line[value] as char;
     result.to_digit(10)
 }
 
 fn str_to_pair(line: &str) -> (u32, u32) {
     let bytes_arr = line.as_bytes();
-    let arr: Vec<u32> = (0..bytes_arr.len())
-        .filter_map(|item| index_to_int(line, item))
-        .collect();
-    (*arr.first().unwrap(), *arr.last().unwrap())
+    let mut iter = (0..bytes_arr.len()).filter_map(|item| index_to_int(bytes_arr, item));
+    let first = iter.next().unwrap();
+    (first, iter.last().unwrap_or(first))
 }
 
 fn pair_to_int(pair: (u32, u32)) -> u32 {
