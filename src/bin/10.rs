36ad4913fdd8eadf63d449d4d55ffe725c674420
Henry Schreiner <henryschreineriii@gmail.com>
2023-12-11 16:22:20 -0500

    feat: add 10b
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 use grid::Grid;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -15,11 +17,11 @@ static DIRECTIONS: [Direction; 4] = [
     Direction::Right,
 ];
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum MapChar {
     Start,
     Vertical,
-    Horizantal,
+    Horizontal,
     UpLeft,
     UpRight,
     DownLeft,
@@ -32,7 +34,7 @@ impl From<char> for MapChar {
         match c {
             'S' => MapChar::Start,
             '|' => MapChar::Vertical,
-            '-' => MapChar::Horizantal,
+            '-' => MapChar::Horizontal,
             'J' => MapChar::UpLeft,
             'L' => MapChar::UpRight,
             '7' => MapChar::DownLeft,
@@ -48,14 +50,14 @@ impl std::fmt::Display for MapChar {
         let s = match self {
             MapChar::Start => "S",
             MapChar::Vertical => "│",
-            MapChar::Horizantal => "─",
+            MapChar::Horizontal => "─",
             MapChar::UpLeft => "╯",
             MapChar::UpRight => "╰",
             MapChar::DownLeft => "╮",
             MapChar::DownRight => "╭",
-            MapChar::Empty => " ",
+            MapChar::Empty => "•",
         };
-        write!(f, "{}", s)
+        write!(f, "{s}")
     }
 }
 
@@ -72,7 +74,7 @@ impl Cursor {
     }
 
     #[must_use]
-    fn step(&self, dir: &Direction) -> Self {
+    fn step(&self, dir: Direction) -> Self {
         match dir {
             Direction::Up => Self::new(self.y - 1, self.x),
             Direction::Down => Self::new(self.y + 1, self.x),
@@ -82,7 +84,7 @@ impl Cursor {
     }
 
     #[must_use]
-    fn peek<'a, T>(&self, grid: &'a Grid<T>, dir: &Direction) -> Option<&'a T> {
+    fn peek<'a, T>(&self, grid: &'a Grid<T>, dir: Direction) -> Option<&'a T> {
         match dir {
             Direction::Up => grid.get(self.y.checked_sub(1)?, self.x),
             Direction::Down => grid.get(self.y + 1, self.x),
@@ -101,67 +103,119 @@ impl Cursor {
     }
 
     #[must_use]
+    fn compute_start_char(&self, grid: &Grid<MapChar>) -> &MapChar {
+        assert!(grid[(self.y, self.x)] == MapChar::Start);
+        match self.get_from_start(grid) {
+            (Direction::Up, Direction::Down) | (Direction::Down, Direction::Up) => {
+                &MapChar::Vertical
+            }
+            (Direction::Left, Direction::Right) | (Direction::Right, Direction::Left) => {
+                &MapChar::Horizontal
+            }
+            (Direction::Up, Direction::Left) | (Direction::Left, Direction::Up) => &MapChar::UpLeft,
+            (Direction::Up, Direction::Right) | (Direction::Right, Direction::Up) => {
+                &MapChar::UpRight
+            }
+            (Direction::Down, Direction::Left) | (Direction::Left, Direction::Down) => {
+                &MapChar::DownLeft
+            }
+            (Direction::Down, Direction::Right) | (Direction::Right, Direction::Down) => {
+                &MapChar::DownRight
+            }
+            _ => panic!("Invalid start"),
+        }
+    }
+
+    #[must_use]
+    fn get_from_start(&self, grid: &Grid<MapChar>) -> (Direction, Direction) {
+        assert!(grid[(self.y, self.x)] == MapChar::Start);
+        let mut valid_dirs = DIRECTIONS.iter().filter(|dir| {
+            log::debug!("{self:?} {dir:?}");
+            if let Some(next) = self.peek(grid, **dir) {
+                log::debug!("Checking ({next}, {dir:?})");
+                matches!(
+                    (next, dir),
+                    (MapChar::Vertical, Direction::Up | Direction::Down)
+                        | (MapChar::Horizontal, Direction::Left | Direction::Right)
+                        | (MapChar::UpLeft, Direction::Right | Direction::Down)
+                        | (MapChar::UpRight, Direction::Left | Direction::Down)
+                        | (MapChar::DownLeft, Direction::Right | Direction::Up)
+                        | (MapChar::DownRight, Direction::Left | Direction::Up)
+                )
+            } else {
+                false
+            }
+        });
+        (*valid_dirs.next().unwrap(), *valid_dirs.next().unwrap())
+    }
+    #[must_use]
     fn find_end(
         &self,
-        start_direction: &Direction,
+        start_direction: Direction,
         grid: &Grid<MapChar>,
         mask: &mut Grid<bool>,
     ) -> usize {
         let mut cursor = self.clone();
-        let mut current_dir = *start_direction;
+        let mut current_dir = start_direction;
         let mut next_dir: Direction;
         for i in 0.. {
             mask[(cursor.y, cursor.x)] = true;
-            next_dir = if let Some(next) = cursor.peek(grid, &current_dir) {
+            next_dir = if let Some(next) = cursor.peek(grid, current_dir) {
                 log::debug!("{i}: {current_dir:?} -> {next}");
                 match (next, current_dir) {
-                    (MapChar::Vertical, Direction::Up) => Direction::Up,
-                    (MapChar::Vertical, Direction::Down) => Direction::Down,
-                    (MapChar::Horizantal, Direction::Left) => Direction::Left,
-                    (MapChar::Horizantal, Direction::Right) => Direction::Right,
-                    (MapChar::UpLeft, Direction::Right) => Direction::Up,
-                    (MapChar::UpLeft, Direction::Down) => Direction::Left,
-                    (MapChar::UpRight, Direction::Down) => Direction::Right,
-                    (MapChar::UpRight, Direction::Left) => Direction::Up,
-                    (MapChar::DownLeft, Direction::Right) => Direction::Down,
-                    (MapChar::DownLeft, Direction::Up) => Direction::Left,
-                    (MapChar::DownRight, Direction::Left) => Direction::Down,
-                    (MapChar::DownRight, Direction::Up) => Direction::Right,
+                    (MapChar::Vertical, Direction::Up)
+                    | (MapChar::UpLeft, Direction::Right)
+                    | (MapChar::UpRight, Direction::Left) => Direction::Up,
+                    (MapChar::Vertical, Direction::Down)
+                    | (MapChar::DownLeft, Direction::Right)
+                    | (MapChar::DownRight, Direction::Left) => Direction::Down,
+                    (MapChar::Horizontal, Direction::Left)
+                    | (MapChar::UpLeft, Direction::Down)
+                    | (MapChar::DownLeft, Direction::Up) => Direction::Left,
+                    (MapChar::Horizontal, Direction::Right)
+                    | (MapChar::UpRight, Direction::Down)
+                    | (MapChar::DownRight, Direction::Up) => Direction::Right,
                     (MapChar::Start, _) => return i,
                     _ => panic!("No next direction found"),
                 }
             } else {
                 panic!("No next direction found");
             };
-            cursor = cursor.step(&current_dir);
+            cursor = cursor.step(current_dir);
             current_dir = next_dir;
         }
         panic!("No end found");
     }
+}
 
-    #[must_use]
-    fn get_next_from_start(&self, grid: &Grid<MapChar>) -> &Direction {
-        DIRECTIONS
-            .iter()
-            .find(|dir| {
-                log::debug!("{self:?} {dir:?}");
-                if let Some(next) = self.peek(grid, dir) {
-                    log::debug!("Checking ({next}, {dir:?})");
-                    matches!(
-                        (next, dir),
-                        (MapChar::Vertical, Direction::Up | Direction::Down)
-                            | (MapChar::Horizantal, Direction::Left | Direction::Right)
-                            | (MapChar::UpLeft, Direction::Right | Direction::Down)
-                            | (MapChar::UpRight, Direction::Left | Direction::Down)
-                            | (MapChar::DownLeft, Direction::Right | Direction::Up)
-                            | (MapChar::DownRight, Direction::Left | Direction::Up)
-                    )
-                } else {
-                    false
+#[must_use]
+fn is_inside(grid: &Grid<MapChar>, mask: &Grid<bool>, loc: &(usize, usize)) -> bool {
+    let (y, min_x) = *loc;
+    let mut crossings = 0;
+    let mut enter = None;
+    for x in (min_x)..(grid.cols()) {
+        if mask[(y, x)] {
+            let mut c = grid[(y, x)];
+            if c == MapChar::Start {
+                c = *Cursor::new(y, x).compute_start_char(grid);
+            }
+            match (c, enter) {
+                (MapChar::Vertical, _) => crossings += 1,
+                (MapChar::DownRight, None) => enter = Some(Direction::Down),
+                (MapChar::UpRight, None) => enter = Some(Direction::Up),
+                (MapChar::DownLeft, Some(Direction::Down))
+                | (MapChar::UpLeft, Some(Direction::Up)) => enter = None,
+                (MapChar::DownLeft, Some(Direction::Up))
+                | (MapChar::UpLeft, Some(Direction::Down)) => {
+                    enter = None;
+                    crossings += 1;
                 }
-            })
-            .unwrap()
+                (MapChar::Horizontal, Some(_)) => (),
+                _ => panic!("Invalid char {c:?} at ({y}, {x}), enter={enter:?}"),
+            }
+        }
     }
+    crossings % 2 == 1
 }
 
 fn compute_and_print_grid(strs: &[&str]) -> (usize, usize) {
@@ -172,15 +226,21 @@ fn compute_and_print_grid(strs: &[&str]) -> (usize, usize) {
         strs[0].len(),
     );
     let mut mask: Grid<bool> = Grid::new(grid.rows(), grid.cols());
+    let mut inside: Grid<bool> = Grid::new(grid.rows(), grid.cols());
 
     let cursor = Cursor::find_start(&grid);
-    let current_dir = cursor.get_next_from_start(&grid);
+    let current_dir = cursor.get_from_start(&grid).0;
     let count = cursor.find_end(current_dir, &grid, &mut mask);
     for (y, row) in grid.iter_rows().enumerate() {
         for (x, c) in row.enumerate() {
+            if !mask[(y, x)] {
+                inside[(y, x)] = is_inside(&grid, &mask, &(y, x));
+            }
             let s = c.to_string();
             if mask[(y, x)] {
                 print!("\x1b[93m{s}\x1b[0m");
+            } else if inside[(y, x)] {
+                print!("\x1b[92m{s}\x1b[0m");
             } else {
                 print!("{s}");
             }
@@ -188,7 +248,7 @@ fn compute_and_print_grid(strs: &[&str]) -> (usize, usize) {
         println!();
     }
 
-    let internal: usize = 0;
+    let internal: usize = inside.iter().map(|x| usize::from(*x)).sum();
     (((count + 1) / 2), internal)
 }
 
@@ -198,8 +258,8 @@ fn main() {
     let grid: Vec<&str> = text.lines().collect();
     let (count, internal) = compute_and_print_grid(&grid);
 
-    println!("Count: {}", count);
-    println!("Internal: {}", internal);
+    println!("Count: {count}");
+    println!("Internal: {internal}");
 }
 
 #[cfg(test)]
