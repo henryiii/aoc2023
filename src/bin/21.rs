daed090bd0f116128d0a9d35eb9f948798e7b5b5
Henry Schreiner <henryschreineriii@gmail.com>
2023-12-22 11:47:30 -0500

    refactor: rewrite 21 to have smarter traversal
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -9,94 +9,120 @@ had to look around for a bit of insperation. I basically just expand and
 compute the first couple of board-lenths (131), and then solve; the puzzle
 boards are all clear around the diagonals and edges, so each expansion adds
 the same amount (partially squared) to the total.
+
+This was rewritten to aboid copying the mask and to avoid grid copies too.  It
+is much, much faster than the original version, and supports arbitray numbers of
+steps.
 */
 
 use grid::Grid;
 
+/// This could be a inline function with `impl TryFrom<i32>`, but making it
+/// a macro allows us to reuse the name, which is fun. This also puts the
+/// error message in the right spot.
+#[macro_export]
+macro_rules! i32 {
+    ($x:expr) => {
+        i32::try_from($x).unwrap()
+    };
+}
+
+#[macro_export]
+macro_rules! usize {
+    ($x:expr) => {
+        usize::try_from($x).unwrap()
+    };
+}
+
 #[must_use]
-fn read(text: &str) -> Grid<i32> {
-    text.lines()
+fn read(text: &str) -> (Grid<bool>, (usize, usize)) {
+    let mask: Grid<bool> = text
+        .lines()
         .map(|line| {
             line.chars()
                 .map(|x| match x {
-                    '#' => -2,
-                    '.' => -1,
-                    'S' => 0,
+                    '#' => false,
+                    '.' | 'S' => true,
                     _ => panic!("Unknown char {x}"),
                 })
                 .collect()
         })
         .collect::<Vec<Vec<_>>>()
-        .into()
+        .into();
+
+    let (y, x) = text
+        .lines()
+        .enumerate()
+        .find_map(|(y, line)| Some((y, line.find('S')?)))
+        .unwrap();
+
+    assert_eq!(y, mask.rows() / 2);
+    assert_eq!(x, mask.cols() / 2);
+    (mask, (y, x))
 }
 
 #[must_use]
-fn expand(grid: &Grid<i32>) -> Grid<i32> {
-    let mut new_grid = Grid::new(grid.rows() * 3, grid.cols() * 3);
-    for i in 0..3 {
-        for j in 0..3 {
-            for ((y, x), v) in grid.indexed_iter() {
-                let new_val = if *v == 0 && (i != 1 || j != 1) {
-                    -1
-                } else {
-                    *v
-                };
-                new_grid[(y + i * grid.rows(), x + j * grid.cols())] = new_val;
-            }
-        }
-    }
-    new_grid
-}
-
-#[must_use]
-fn steps(grid: Grid<i32>, dist: usize) -> Grid<i32> {
-    let mut grid = grid;
-    for i in 0..i32::try_from(dist).unwrap() {
-        let mut new_grid = grid.clone();
-        for y in 0..grid.rows() {
-            for x in 0..grid.cols() {
-                if grid[(y, x)] == i {
+fn steps(mask: &Grid<bool>, start: &(usize, usize), dist: usize) -> Grid<bool> {
+    let mut grid: Grid<bool> = Grid::new(2 * dist + 1, 2 * dist + 1);
+    let midpoint = (i32!(grid.rows() - 1) / 2, i32!(grid.cols() - 1) / 2);
+    let start = (i32!(start.0), i32!(start.1));
+    grid[(usize!(midpoint.0), usize!(midpoint.1))] = true;
+    for i in 0..i32!(dist) {
+        // y, x are distances from the center, in grid coords
+        for y in -i..=i {
+            for x in -i..=i {
+                // In order to avoid copies, we can use a checkerboard pattern
+                if i % 2 != (x + y).rem_euclid(2) {
+                    continue;
+                }
+                // yc, xc are distance from the corner, grid coords
+                let yc = y + midpoint.0;
+                let xc = x + midpoint.1;
+                // If this value is true, we swap with four surrounding values
+                if *grid.get(yc, xc).unwrap() {
                     for (dy, dx) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                        let ty = i32::try_from(y).unwrap() + dy;
-                        let tx = i32::try_from(x).unwrap() + dx;
+                        // ty, tx are the new values, in grid coords
+                        let ty = yc + dy;
+                        let tx = xc + dx;
 
-                        if let Some(v) = grid.get(ty, tx) {
-                            if *v >= -1 {
-                                *new_grid.get_mut(ty, tx).unwrap() = i + 1;
-                                new_grid[(y, x)] = -1;
-                            }
+                        // my, mx is the new values, in mask coords
+                        let my = usize!((ty + start.0 - midpoint.0).rem_euclid(i32!(mask.rows())));
+                        let mx = usize!((tx + start.1 - midpoint.1).rem_euclid(i32!(mask.cols())));
+
+                        if mask[(my, mx)] {
+                            grid[(usize!(ty), usize!(tx))] = true;
+                            grid[(usize!(yc), usize!(xc))] = false;
                         }
                     }
                 }
             }
         }
-        grid = new_grid;
     }
     grid
 }
 
 #[must_use]
-fn count_locations(grid: &Grid<i32>) -> usize {
-    grid.iter().filter(|x| **x > 0).count()
+fn count_locations(grid: &Grid<bool>) -> usize {
+    grid.iter().filter(|x| **x).count()
 }
 
-fn count_block(grid: &Grid<i32>, is_odd: bool) -> usize {
-    grid.indexed_iter()
-        .filter(|((y, x), v)| **v != -2 && (y + x) % 2 == usize::from(is_odd))
-        .count()
-}
-
-fn print_grid(grid: &Grid<i32>) {
-    for row in grid.iter_rows() {
-        for x in row {
-            let c = match x {
-                -2 => '#',
-                -1 => '.',
-                0 => 'S',
-                1.. => 'O',
-                _ => panic!("Unknown char {x}"),
+fn print_grid(mask: &Grid<bool>, grid: &Grid<bool>, start: &(usize, usize)) {
+    let midpoint = ((grid.rows() - 1) / 2, (grid.cols() - 1) / 2);
+    for yc in 0..grid.rows() {
+        for xc in 0..grid.cols() {
+            let y = (i32!(yc + start.0) - i32!(midpoint.0)).rem_euclid(i32!(mask.rows()));
+            let x = (i32!(xc + start.1) - i32!(midpoint.1)).rem_euclid(i32!(mask.cols()));
+            let c = match (mask[(usize!(y), usize!(x))], grid[(yc, xc)]) {
+                (false, false) => '#',
+                (true, false) => '.',
+                (true, true) => 'O',
+                (false, true) => panic!("Can't exist in both at {y}, {x}!"),
             };
-            print!("{c}");
+            if yc == grid.rows() / 2 && xc == grid.cols() / 2 {
+                print!("S");
+            } else {
+                print!("{c}");
+            }
         }
         println!();
     }
@@ -104,15 +130,10 @@ fn print_grid(grid: &Grid<i32>) {
 
 #[must_use]
 fn compute1(text: &str) -> usize {
-    let grid = read(text);
-    let grid = steps(grid, 64);
-    print_grid(&grid);
+    let (mask, start) = read(text);
+    let grid = steps(&mask, &start, 64);
+    print_grid(&mask, &grid, &start);
     count_locations(&grid)
-}
-
-fn compute2(text: &str) -> usize {
-    let grid = read(text);
-    count_block(&grid, true)
 }
 
 fn main() {
@@ -120,26 +141,19 @@ fn main() {
     let result = compute1(&text);
     println!("First = {result}");
 
-    let result = compute2(&text);
-    println!("Full block (odd steps) = {result}");
     let val = 26_501_365;
-    println!("Steps = {val}");
+    println!("Going to compute for steps = {val}");
 
-    let grid = read(&text);
-    let grid = steps(grid, 65);
+    let (mask, start) = read(&text);
+    let grid = steps(&mask, &start, 65);
     let y_0 = count_locations(&grid);
     println!("131*0 = 65 = {y_0}");
 
-    let grid = read(&text);
-    let grid = expand(&grid);
-    let grid = steps(grid, 65 + 131);
+    let grid = steps(&mask, &start, 65 + 131);
     let y_1 = count_locations(&grid);
     println!("131*1 + 65 = {y_1}");
 
-    let grid = read(&text);
-    let grid = expand(&grid);
-    let grid = expand(&grid);
-    let grid = steps(grid, 65 + 131 * 2);
+    let grid = steps(&mask, &start, 65 + 131 * 2);
     let y_2 = count_locations(&grid);
     println!("131*2 + 65 = {y_2}");
 
@@ -155,7 +169,6 @@ fn main() {
         "x=202300, y={}",
         (202_300 * 202_300 * a2 + 202_300 * b2) / 2 + c
     );
-    // 11842978506936380 too high
 }
 
 #[cfg(test)]
@@ -177,20 +190,26 @@ mod tests {
 
     #[test]
     fn test_first() {
-        let grid = read(INPUT);
-        let grid = steps(grid, 6);
-        print_grid(&grid);
+        let (mask, start) = read(INPUT);
+        let grid = steps(&mask, &start, 6);
         let result = count_locations(&grid);
+        print_grid(&mask, &grid, &start);
         assert_eq!(result, 16);
     }
 
     #[test]
-    fn test_expand() {
-        let grid = read(INPUT);
-        let grid = expand(&grid);
-        let grid = steps(grid, 6);
-        print_grid(&grid);
+    fn test_second() {
+        let (mask, start) = read(INPUT);
+        let grid = steps(&mask, &start, 10);
         let result = count_locations(&grid);
-        assert_eq!(result, 16);
+        assert_eq!(result, 50);
+    }
+
+    #[test]
+    fn test_third() {
+        let (mask, start) = read(INPUT);
+        let grid = steps(&mask, &start, 50);
+        let result = count_locations(&grid);
+        assert_eq!(result, 1594);
     }
 }
